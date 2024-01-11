81348b4253daf0b60738d80c73fe6fdf1654415d
Pedro <pramos.6.2000@gmail.com>
2024-01-11 12:17:54 +0000

    asda

--- app/src/main/java/pt/pedro/ccti/weatherapp/utils/Constants.kt
+++ app/src/main/java/pt/pedro/ccti/weatherapp/utils/Constants.kt
@@ -2,5 +2,5 @@ package pt.pedro.ccti.weatherapp.utils
 
 object Constants {
     const val   BASE_URL = "https://api.weatherapi.com"
-    const val API_KEY = "f49d2695134940cda9f11127231812"
+    const val API_KEY = "2f08d8ccc7a245b5b2e192429240201"
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
+    val agp_version by extra("8.1.3")
     repositories {
         google()
         mavenCentral()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.3")  // Updated version
+        classpath("com.android.tools.build:gradle:$agp_version")  // Updated version
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")  // Updated version
         classpath("com.google.gms:google-services:4.4.0")  // Your version is fine
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")  // Your version is fine
